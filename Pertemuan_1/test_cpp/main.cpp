adc2e4aa1bd9b137c8ab866a3606dc07301fbcf9
Virtuozs <taufiqurrahman6595@gmail.com>
2024-09-27 11:02:39 +0700

    Setting a default compiler for run and debugging

--- Pertemuan_1/test_cpp/main.cpp
+++ Pertemuan_1/test_cpp/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    int a, b;
-    cin >> a >> b;
-    cout << a*b;
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+
     return 0;
 }
